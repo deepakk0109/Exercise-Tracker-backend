f9becaccd1662083e99d11cd5d371a885b9bce79
Deepak Kumar <106617671+deepakk0109@users.noreply.github.com>
2024-01-26 22:43:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:3000","https://exercise-tracker-app-yxsp.onrender.com"],
+}));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
